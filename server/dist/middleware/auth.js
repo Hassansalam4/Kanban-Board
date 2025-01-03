524412d8ca380ed9c29eee20bf417ab06003b7c9
Hassansalam4 <atuman4@gmail.com>
2025-01-03 15:28:09 -0500

    fix connection issues to postgres

--- server/dist/middleware/auth.js
+++ server/dist/middleware/auth.js
@@ -1,11 +1,13 @@
 import jwt from 'jsonwebtoken';
 export const authenticateToken = (req, res, next) => {
-    // TODO: verify the token exists and add the user data to the request object
     const token = req.headers['authorization'];
-    if (token == null)
-        return res.sendStatus(401);
-    jwt.verify(token, process.env.JWT_SECRET_KEY || "", function (err, decoded) {
-        // err
-        // decoded undefined
-    });
+    if (token) {
+        jwt.verify(token, process.env.JWT_SECRET_KEY || "", function (err, decoded) {
+            if (err)
+                return res.sendStatus(403);
+            req.user = decoded; // Add user data to the request object
+            return next();
+        });
+    }
+    return res.sendStatus(401);
 };
--- server/src/middleware/auth.ts
+++ server/src/middleware/auth.ts
@@ -6,11 +6,15 @@ interface JwtPayload {
 }
 
 export const authenticateToken = (req: Request, res: Response, next: NextFunction) => {
-  // TODO: verify the token exists and add the user data to the request object
- const token = req.headers['authorization'];
-  if (token == null) return res.sendStatus(401);
-  jwt.verify(token, process.env.JWT_SECRET_KEY||"", function(err, decoded) {
-    // err
-    // decoded undefined
-  });
+  const token = req.headers['authorization'];
+  if (token) {
+    jwt.verify(token, process.env.JWT_SECRET_KEY || "", function(err, decoded) {
+      if (err) return res.sendStatus(403);
+      req.user = decoded as JwtPayload; // Add user data to the request object
+      return next();
+    });
+  }
+  return res.sendStatus(401);
+
+  
 };
