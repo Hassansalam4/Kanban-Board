0d41109a4a8254208d2dad36fdb6dda653717cdc
Hassansalam4 <atuman4@gmail.com>
2025-01-06 22:23:33 -0500

    add read.me

--- client/src/api/authAPI.tsx
+++ client/src/api/authAPI.tsx
@@ -1,9 +1,9 @@
 import { UserLogin } from "../interfaces/UserLogin";
 
-const login = async (userInfo: UserLogin) => {
+ const login = async (userInfo: UserLogin) => {
   // TODO: make a POST request to the login route
   try { 
-    const response = await fetch('/api/auth/login', {
+    const response = await fetch('/auth/login', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
